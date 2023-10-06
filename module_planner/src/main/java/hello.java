9847a117e837b61508a59a0db5b0d812d929fe7d
ryu <fbqudrlf09@naver.com>
2023-10-06 13:39:44 +0900

    1. build.gradle 작성 2. gitIgnore 추가

---
+++ module_planner/src/main/java/hello.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
